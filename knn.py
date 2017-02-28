b869424480ee5f1ddca72c53578defee6e8bf3cb
Ken Li <iliawnek@gmail.com>
2017-02-28 17:29:49 +0000

    Use multi-processing for cross-validation to improve performance

--- knn.py
+++ knn.py
@@ -1,34 +1,31 @@
 import numpy as np
-# import pylab as plt
 import csv
+import multiprocessing as mp
+
 
 # Get data from tsv file.
-
 X = []
 T = []
 
 with open('epi_stroma_data.tsv', 'rb') as tsv:
     i = 0
-    keys = []
     for line in csv.reader(tsv, delimiter='\t'):
         if i != 0:
             X.append([float(x) for x in line[1:]])
-            T.append([float(line[0])])
+            T.append([int(line[0])])
         i += 1
 
 X = np.array(X)
 T = np.array(T)
 
 data_point_count = X.shape[0]
-feature_count = X.shape[1]
 
-# Perform cross-validation.
 
-def classify(X, t, x_new, K):
+def classify(X, T, x_new, K):
     # compute distance-class mappings
     distances = []
     for i, x in enumerate(X):
-        distances.append((np.linalg.norm(x_new - X[i]), t[i]))
+        distances.append((np.linalg.norm(x_new - X[i]), T[i]))
 
     # find most popular class in K-nearest neighbours
     distances = sorted(distances, key=lambda d: d[0])
@@ -44,15 +41,18 @@ def classify(X, t, x_new, K):
     else:
         return 2
 
-max_K = 5  # K from 1 to max_K
+min_K = 1
+max_K = 9  # K from 1 to max_K
+step_K = 1
 fold_count = 23
 fold_size = data_point_count / fold_count
-errors = np.zeros(max_K)
+errors = {}
 
-for K in range(1, max_K + 1):
+for K in range(min_K, max_K + 1, step_K):
     error_count = 0
     for fold in range(fold_count):
         print 'K = %d, fold = %d' % (K, fold + 1)
+
         # divide folds into validation and training data
         lower = fold_size * fold
         upper = fold_size * (fold + 1)
@@ -61,11 +61,21 @@ for K in range(1, max_K + 1):
         T_fold = T[lower:upper]
         T_train = np.delete(T, np.arange(lower, upper, 1), 0)
 
-        for x, t in zip(X_fold, T_fold):
-            classification = classify(X_train, T_train, x, K)
-            if classification != t:
-                error_count += 1
+        def count_error(i):
+            classification = classify(X_train, T_train, X_fold[i], K)
+            if classification != T_fold[i][0]:
+                return 1
+            return 0
 
-    errors[K - 1] = error_count / fold_count
+        pool = mp.Pool(4)
+        results = pool.map(count_error, range(fold_size))
+        error_count += sum(results)
+
+    average_error = error_count / fold_count
+    print '---'
+    print 'K = %d, error = %d' % (K, average_error)
+    print '---'
+    errors[K] = average_error
 
 print errors
+
